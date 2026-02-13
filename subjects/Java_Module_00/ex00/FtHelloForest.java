1788469888c89f9bf5b32f4b5f087018ea480e53
Sergio Alejandro <sergioromero2k@gmail.com>
2026-02-13 12:06:11 +0100

    you stayed ex01

---
+++ subjects/Java_Module_00/ex00/FtHelloForest.java
@@ -0,0 +1,25 @@
+package ex00;
+
+/**
+ * Main class for the first exercises of Module 00.
+ */
+
+public class FtHelloForest {
+  /**
+   * Prints a welcome message to the Forest Management System.
+   */
+  public static void ftHelloForest() {
+    System.out.println("Welcome to the Forest Management System!");
+  }
+
+  /**
+   * Main Method.
+   *
+   * @param args arguments.
+   */
+
+  public static void main(String[] args) {
+    FtHelloForest.ftHelloForest();
+  }
+}
+
