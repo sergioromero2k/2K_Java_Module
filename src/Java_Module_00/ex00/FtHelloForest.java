590e00f915b1515aae4d562d75af4cdf368a56b1
Sergio Alejandro <sergioromero2k@gmail.com>
2026-02-13 11:11:15 +0100

    testing intellij

---
+++ src/Java_Module_00/ex00/FtHelloForest.java
@@ -0,0 +1,13 @@
+package Java_Module_00.ex00;
+
+public class FtHelloForest {
+
+    public void ft_hello_forest() {
+        System.out.println("Welcome to the Forest Management System!");
+    }
+
+    public static void main(String[] args){
+        FtHelloForest forest = new FtHelloForest();
+        forest.ft_hello_forest();
+    }
+}
