fdd0449169e96eea0f523d79a7b3115051c8da91
Sergio Alejandro <sergioromero2k@gmail.com>
2026-07-17 00:40:02 +0200

    adding more exercesises

--- src/ex4/SpellFilter.java
+++ src/ex4/SpellFilter.java
@@ -1,5 +1,34 @@
 package ex4;
 
+import java.util.Comparator;
+import java.util.List;
+
+/**
+ * Filters, removes duplicates, and sorts a list of spells.
+ */
 public class SpellFilter {
 
+  /**
+   * Main method that processes the spell list.
+   *
+   * @param args program arguments
+   */
+  public static void main(String[] args) {
+    List<String> hechizos = List.of(
+        "Fireball",
+        "Teleport",
+        "Ice Blast",
+        "Fireball",
+        "Shield",
+        "Teleport",
+        "Lightning"
+    );
+
+    System.out.println("Unique and sorted spells:\n");
+
+    hechizos.stream()
+        .distinct()
+        .sorted(Comparator.naturalOrder())
+        .forEach(System.out::println);
+  }
 }
